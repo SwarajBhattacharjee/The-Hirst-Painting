6b39df252a2e1ccd699c9ae8c3367b48f353c979
SwarajBhattacharjee <swarajbhattacharjee0@gmail.com>
2025-06-30 21:06:57 +0530

    Created the turtle object

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # Create a list of conataining tuples of rgb vallues oof the most used colors in a jpg image
-import colorgram
+# import colorgram
 
-colors = colorgram.extract("The-Hirst-Painting/test-image.jpg", 10)
+# colors = colorgram.extract("The-Hirst-Painting/test-image.jpg", 30)
 
-rgb_colors = []
-for color in colors:
-    rgb = color.rgb  # NamedTuple of (r, g, b)
-    rgb_colors.append((rgb.r, rgb.g, rgb.b))
+# rgb_colors = []
+# for color in colors:
+#     rgb = color.rgb  # NamedTuple of (r, g, b)
+#     rgb_colors.append((rgb.r, rgb.g, rgb.b))
 
-print(rgb_colors)
+# print(rgb_colors)
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+tim.pensize(20)
+tim.shape("circle")
+
+colors_list = [(249, 228, 17), (213, 13, 9), (198, 12, 35), (231, 228, 5), (197, 69, 20), (33, 90, 188), (43, 212, 71), (234, 148, 40), (33, 30, 152), (16, 22, 55), (66, 9, 49), (240, 245, 251), (244, 39, 149), (65, 202, 229), (14, 205, 222), (63, 21, 10), (224, 19, 111), (229, 165, 8), (15, 154, 22), (245, 58, 16), (98, 75, 9), (248, 11, 9), (222, 140, 203), (68, 240, 161), (10, 97, 62), (5, 38, 33), (68, 219, 155)]
